e42192f2fc786a7246de276510f317ca9f87fe18
0xera <gugafasa+1@gmail.com>
2022-03-07 17:26:27 +0300

    sample

--- app/src/main/java/com/example/lecture5/MainActivity.kt
+++ app/src/main/java/com/example/lecture5/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.lecture5
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var inputText: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.text).text = inputText
+        findViewById<EditText>(R.id.edit_text).addTextChangedListener(
+            onTextChanged = { charSeq, _, _, _ ->
+                inputText = charSeq.toString()
+            })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        inputText = persistentState?.getString("PersistKey").orEmpty()
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        log("onConfigurationChanged: $newConfig")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("SampleKey", "SampleValue")
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        outPersistentState.putString("PersistKey", inputText)
+        super.onSaveInstanceState(outState, outPersistentState)
     }
 }
+
+
