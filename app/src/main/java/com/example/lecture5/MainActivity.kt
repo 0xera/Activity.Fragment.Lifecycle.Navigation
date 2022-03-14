e946a98cfc6ce974948299a081198e0f92b00898
0xera <gugafasa+1@gmail.com>
2022-03-14 23:33:36 +0300

    sample

--- app/src/main/java/com/example/lecture5/MainActivity.kt
+++ app/src/main/java/com/example/lecture5/MainActivity.kt
@@ -3,44 +3,45 @@ package com.example.lecture5
 import android.content.res.Configuration
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.addTextChangedListener
 
 
 class MainActivity : AppCompatActivity() {
 
     private var inputText: String = ""
 
+    //    findViewById<TextView>(R.id.text).text = inputText
+//    findViewById<EditText>(R.id.edit_text).addTextChangedListener(
+//    onTextChanged = { charSeq, _, _, _ ->
+//        inputText = charSeq.toString()
+//    })
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<TextView>(R.id.text).text = inputText
-        findViewById<EditText>(R.id.edit_text).addTextChangedListener(
-            onTextChanged = { charSeq, _, _, _ ->
-                inputText = charSeq.toString()
-            })
+//        val savedText = savedInstanceState?.getString("SampleKey")
     }
 
+
+    override fun onSaveInstanceState(outState: Bundle) {
+//        outState.putString("SampleKey", "SampleValue")
+        super.onSaveInstanceState(outState)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        inputText = persistentState?.getString("PersistKey").orEmpty()
+        val text = persistentState?.getString("PersistKey")
         super.onCreate(savedInstanceState, persistentState)
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        outPersistentState.putString("PersistKey", "PersistValue")
+        super.onSaveInstanceState(outState, outPersistentState)
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         log("onConfigurationChanged: $newConfig")
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString("SampleKey", "SampleValue")
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        outPersistentState.putString("PersistKey", inputText)
-        super.onSaveInstanceState(outState, outPersistentState)
     }
 }
 
