4f14e475be3e7d709cc50bfb5e74961b40822dff
0xera <gugafasa+1@gmail.com>
2022-03-20 02:28:02 +0300

    prepare CounterFragment

--- app/src/main/java/com/example/lecture5/CounterFragment.kt
+++ app/src/main/java/com/example/lecture5/CounterFragment.kt
@@ -4,31 +4,23 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 
 class CounterFragment : Fragment(R.layout.fragment_counter) {
 
+    private val args by navArgs<CounterFragmentArgs>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val countTextView = view.findViewById<TextView>(R.id.count_text_view)
-        val addButton = view.findViewById<Button>(R.id.go_to_counter_button)
+        val addButton = view.findViewById<Button>(R.id.add_button)
 
-        var currentValue = arguments?.getInt(CURRENT_VALUE) ?: 0
+        var currentValue = args.currentValue
         countTextView.text = currentValue.toString()
         addButton.setOnClickListener {
             currentValue++
             countTextView.text = currentValue.toString()
         }
 
-    }
-
-    companion object {
-        private const val CURRENT_VALUE = "CURRENT_VALUE"
-
-        fun newInstance(currentValue: Int): CounterFragment {
-            return CounterFragment().apply {
-                arguments = bundleOf(CURRENT_VALUE to currentValue)
-            }
-        }
     }
 }
