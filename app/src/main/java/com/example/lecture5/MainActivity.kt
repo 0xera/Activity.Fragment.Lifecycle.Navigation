70cd3e733e3a0e083473363dbb73e88ffa9287e2
0xera <gugafasa+1@gmail.com>
2022-03-23 02:29:38 +0300

    add to back stack checkbox

--- app/src/main/java/com/example/lecture5/MainActivity.kt
+++ app/src/main/java/com/example/lecture5/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.lecture5
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Button
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
+import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
@@ -24,40 +26,41 @@ class MainActivity : AppCompatActivity() {
             count = savedInstanceState.getInt(COUNT_KEY, 0)
         }
 
+        val checkBox = findViewById<CheckBox>(R.id.backstack_checkbox)
         findViewById<Button>(R.id.replace_button).setOnClickListener {
             supportFragmentManager.commit {
                 replace(R.id.container, SimpleFragment.newInstance(), (++count).toString())
-                addToBackStack(null)
+                addToBackStack(checkBox.isChecked)
             }
         }
         findViewById<Button>(R.id.remove_button).setOnClickListener {
             supportFragmentManager.commit {
                 remove(supportFragmentManager.fragments.last())
-                addToBackStack(null)
+                addToBackStack(checkBox.isChecked)
             }
         }
         findViewById<Button>(R.id.add_button).setOnClickListener {
             supportFragmentManager.commit {
                 add(R.id.container, SimpleFragment.newInstance(), (++count).toString())
-                addToBackStack(null)
+                addToBackStack(checkBox.isChecked)
             }
         }
         findViewById<Button>(R.id.detach_button).setOnClickListener {
             supportFragmentManager.commit {
                 detach(supportFragmentManager.fragments.last())
-                addToBackStack(null)
+                addToBackStack(checkBox.isChecked)
             }
         }
         findViewById<Button>(R.id.hide_button).setOnClickListener {
             supportFragmentManager.commit {
                 hide(supportFragmentManager.fragments.last())
-                addToBackStack(null)
+                addToBackStack(checkBox.isChecked)
             }
         }
         findViewById<Button>(R.id.show_button).setOnClickListener {
             supportFragmentManager.commit {
                 show(supportFragmentManager.fragments.last())
-                addToBackStack(null)
+                addToBackStack(checkBox.isChecked)
             }
         }
         findViewById<Button>(R.id.pop_button).setOnClickListener {
@@ -68,6 +71,10 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun FragmentTransaction.addToBackStack(isChecked: Boolean) =
+        if (isChecked) addToBackStack(null) else this
+
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
         outState.putInt(COUNT_KEY, count)
